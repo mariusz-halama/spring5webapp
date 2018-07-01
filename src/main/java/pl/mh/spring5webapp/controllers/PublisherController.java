4ae39050559a5e1e817b6601086d292ac7f9b6f5
Mariusz Halama <mariusz.halama@gmail.com>
2018-07-01 21:56:31 +0200

    Added controllers and templates

---
+++ src/main/java/pl/mh/spring5webapp/controllers/PublisherController.java
@@ -0,0 +1,24 @@
+package pl.mh.spring5webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.mh.spring5webapp.repositories.PublisherRepository;
+
+@Controller
+public class PublisherController {
+
+    private PublisherRepository publisherRepository;
+
+    public PublisherController(PublisherRepository publisherRepository) {
+        this.publisherRepository = publisherRepository;
+    }
+
+    @RequestMapping("/publishers")
+    public String getPublishers(Model model) {
+
+        model.addAttribute("publishers", publisherRepository.findAll());
+
+        return "publishers";
+    }
+}
